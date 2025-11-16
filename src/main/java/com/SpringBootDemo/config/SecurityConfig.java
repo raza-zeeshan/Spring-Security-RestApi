1c8c239b38b866d95eb57f8bde29ac6cc7a05b58
Zeeshan Raza <zeeshanrahman72@gmail.com>
2025-11-16 12:57:28 +0530

    Security config file modified

--- src/main/java/com/SpringBootDemo/config/SecurityConfig.java
+++ src/main/java/com/SpringBootDemo/config/SecurityConfig.java
@@ -6,6 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -15,25 +21,24 @@ public class SecurityConfig {
 	@ConditionalOnMissingBean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-		http.authorizeHttpRequests(
-				auth -> auth.requestMatchers("/about", "/contact", "/welcome").permitAll().anyRequest().authenticated())
+		http.authorizeHttpRequests(auth -> auth.requestMatchers("/about", "/contact", "/welcome").permitAll()
+				.requestMatchers("/home", "/deposit", "/balance").hasAnyRole("admin", "user")
+				.requestMatchers("/loan", "/change").hasRole("admin").anyRequest().authenticated())
 				.formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
 		return http.build();
-//		
-//		.requestMatchers("/home", "/deposit", "/balance").hasAnyRole("admin", "user")
-//		.requestMatchers("/loan", "/change").hasRole("admin")
+
 	}
 
-//	@Bean
-//	public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-//		UserDetails u1 = User.withUsername("Zeeshan").password(encoder.encode("12345678")).roles("admin").build();
-//		UserDetails u2 = User.withUsername("Anupam").password(encoder.encode("87654321")).roles("user").build();
-//		return new InMemoryUserDetailsManager(u1, u2);
-//	}
+	@Bean
+	public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+		UserDetails u1 = User.withUsername("Zeeshan").password(encoder.encode("12345678")).roles("admin").build();
+		UserDetails u2 = User.withUsername("Anupam").password(encoder.encode("87654321")).roles("user").build();
+		return new InMemoryUserDetailsManager(u1, u2);
+	}
 
-//	@Bean
-//	public PasswordEncoder psEncoder() {
-//		return new BCryptPasswordEncoder();
-//	}
+	@Bean
+	public PasswordEncoder psEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
 }
